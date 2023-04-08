75a2dbcb385b1cf21c96922f8667c000654c1cbe
Revenant <dasdev.pratik@gmail.com>
2023-04-09 00:46:49 +0530

    name update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import Carousel from "../components/Carousel";
+import Carousel from "../components/Home/Carousel";
 import Navbar from "../components/Navbar";
-import Cards from "../components/Cards";
-import Appointment from "../components/Appointment";
-import Footer from "../components/Footer";
+import Cards from "../components/Home/Cards";
+import Appointment from "../components/Home/Appointment";
+import Footer from "../components/Home/Footer";
 import { Outlet } from "react-router-dom";
 
 export const Home = () => {
